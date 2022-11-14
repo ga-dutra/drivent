39de4e66c772ca935077964b4e78b5e7912f2e5e
gacdutra <ga.acdutra@gmail.com>
2022-11-14 14:06:31 -0300

    refactor: type ViaCEPAddress

--- src/protocols.ts
+++ src/protocols.ts
@@ -4,17 +4,17 @@ export type ApplicationError = {
 };
 
 export type ViaCEPAddress = {
-  logradouro: string,
-  complemento: string,
-  bairro: string,
-  localidade: string,
-  uf: string,
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  cidade: string;
+  uf: string;
 };
 
 export type RequestError = {
-  status: number,
-  data: object | null,
-  statusText: string,
-  name: string,
-  message: string,
+  status: number;
+  data: object | null;
+  statusText: string;
+  name: string;
+  message: string;
 };
