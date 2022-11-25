ce2a9339c0d0172c010b29a9472620b3c010c131
gacdutra <ga.acdutra@gmail.com>
2022-11-25 17:07:14 -0300

    feat: added createRoom function

--- tests/factories/hotels-factory.ts
+++ tests/factories/hotels-factory.ts
@@ -2,10 +2,20 @@ import faker from "@faker-js/faker";
 import { prisma } from "@/config";
 
 export async function createHotel() {
-  return prisma.hotel.create({
+  return await prisma.hotel.create({
     data: {
       name: faker.name.findName(),
       image: faker.image.image(),
+    }
+  });
+}
+
+export async function createRoom(hotelId: number) {
+  return await prisma.room.create({
+    data: {
+      name: faker.name.findName(),
+      capacity: faker.datatype.number({ min: 1, max: 999 }),
+      hotelId
     }
   });
 }
